e4276cea31d8976789c41dfa009cfd4edff77df2
4Vitalii <4vo.ivanov@gmail.com>
2023-07-02 10:13:16 +0300

    удален pom, исправлен цикл

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,8 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
